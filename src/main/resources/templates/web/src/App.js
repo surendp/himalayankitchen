29a41a01e6a5230371070b308ef38e41c4e981dd
SubroBlack <dahalsubodh8@gmail.com>
2018-01-11 22:40:11 +0200

    Added Web Components including Header, Title bar with logo and rough construction start for body. Head and title need to be fitted and checked for different mobile dimensions

--- src/main/resources/templates/web/src/App.js
+++ src/main/resources/templates/web/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/appHeader.jsx';
+import Title from './components/title.jsx';
+import Body from './components/appBody.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />;
+        <Title />;
+        <Body />;
       </div>
     );
   }
---
+++ src/main/resources/templates/web/src/components/appBody.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import bg1 from '../media/bg1.jpg';
+import '../styles/appBody.css';
+
+class appBody extends React.Component {
+  render() {
+    return (
+      <div className="app-Body">
+        <h1>Hello</h1>
+        <h3>Expetendis ea quae, de qui sunt mandaremus.
+
+          Ingeniis quid offendit ullamco a minim admodum ubi offendit.
+
+          Ut sed dolor eiusmod, magna officia sed coniunctione.
+
+          Senserit sunt sint in amet, ipsum quo sed quae aliquip.
+
+          Legam hic admodum id fore, de nescius est cernantur.
+
+          Mandaremus nam illum quamquam, aut duis dolor et nescius.
+
+          Eu velit coniunctione si anim quamquam coniunctione.
+
+          Tempor qui id nulla occaecat si eiusmod quae export hic velit.
+
+          Fore nostrud a aliquip a sed nulla eiusmod doctrina.</h3>
+      </div>
+    )
+  }
+}
+
+
+export default appBody;
