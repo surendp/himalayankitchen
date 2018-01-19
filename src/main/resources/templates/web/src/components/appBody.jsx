67fed3ce6f9a9de78aa22c2c943bd90c0f922715
SubroBlack <dahalsubodh8@gmail.com>
2018-01-19 12:12:27 +0200

    Added images in the background

--- src/main/resources/templates/web/src/components/appBody.jsx
+++ src/main/resources/templates/web/src/components/appBody.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import bg1 from '../media/bg1.jpg';
 import '../styles/appBody.css';
+import HomeContent from './homeContent.jsx';
+import bg1 from '../media/bg1.jpg';
+import bg2 from '../media/bg2.jpg';
+import bg3 from '../media/bg3.jpg';
 
 class appBody extends React.Component {
   render() {
     return (
       <div className="app-Body">
-        <h1>Hello</h1>
-        <h3>Expetendis ea quae, de qui sunt mandaremus.
-
-          Ingeniis quid offendit ullamco a minim admodum ubi offendit.
-
-          Ut sed dolor eiusmod, magna officia sed coniunctione.
-
-          Senserit sunt sint in amet, ipsum quo sed quae aliquip.
-
-          Legam hic admodum id fore, de nescius est cernantur.
-
-          Mandaremus nam illum quamquam, aut duis dolor et nescius.
-
-          Eu velit coniunctione si anim quamquam coniunctione.
-
-          Tempor qui id nulla occaecat si eiusmod quae export hic velit.
-
-          Fore nostrud a aliquip a sed nulla eiusmod doctrina.</h3>
+        <img src={bg1} className="slides" alt="logo" />
+        <HomeContent />
+        <img src={bg2} className="slides" alt="logo" />
+        <img src={bg3} className="slides" alt="logo" />
       </div>
     )
   }
