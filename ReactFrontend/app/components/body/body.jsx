21f86f180fee37817f3f37c1d9917ee8f4fa1af3
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-01-27 16:05:25 +0200

    About us content added in the mobile website. contentDiv.jsx created as the helper component

--- ReactFrontend/app/components/body/body.jsx
+++ ReactFrontend/app/components/body/body.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../../css/body.css';
+import AboutUs from './aboutUs.jsx';
 
 class Body extends React.Component{
   render(){
     return (
-        <div className="styleMainBodyDiv">Body</div>
+        <div className="styleMainBodyDiv">
+            <AboutUs />
+        </div>
     );
   }
 }
