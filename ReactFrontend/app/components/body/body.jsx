e986a50185bbc40002286560fa7104ae4c7370d5
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-01-27 19:15:49 +0200

    homedelivery , info and location added

--- ReactFrontend/app/components/body/body.jsx
+++ ReactFrontend/app/components/body/body.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../../css/body.css';
+
 import AboutUs from './aboutUs.jsx';
+import Location from './location.jsx';
+import Info from './info.jsx';
+import HomeDelivery from './homeDelivery.jsx';
 
 class Body extends React.Component{
   render(){
     return (
-        <div className="styleMainBodyDiv">
+        <div className="styleMainBodyDiv styleBackground">
             <AboutUs />
+            <Location />
+            <Info />
+            <HomeDelivery />
         </div>
     );
   }
