3239c1b0f2b39cd2663e9c9ba2a02f4e6ffbf721
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-03-02 21:58:27 +0200

    form submission to an email address successfully implemented

--- ReactFrontend/app/components/body/reservationForm/submitForm.js
+++ ReactFrontend/app/components/body/reservationForm/submitForm.js
@@ -6,19 +6,34 @@
 
 class SubmitForm{
 
+  // constructor takes formData object as input
   constructor(formData){
     this.formData = formData;
     this.message  = this.createMessage();
   }
 
+  // create a string message from the form data
   createMessage(){
-      let message = "hello";
+      let message =  ((this.formData.name        !=="") ? ("Mr/Ms "+this.formData.name+ " wants to make a reservation,") : "")
+                    +((this.formData.partySize   !=="") ? ("for "+this.formData.partySize+" people.\n") : "")
+                    +((this.formData.email       !=="") ? ("Email: "+ this.formData.email+"\n") :"")
+                    +((this.formData.phoneNumber !=="") ? ("Contact number: "+this.formData.phoneNumber+"\n") : "")
+                    +((this.formData.date        !=="") ? ("Date: "+ this.formData.date+"\n") : "")
+                    +((this.formData.message     !=="") ? ("Message: "+this.formData.message) : "");
 
       return message;
   }
 
+  // submitData method takes api as parameter
+  // and makes post request to the api with
+  // message property as body of the request
   submitData(api){
-    //console.log(api);
+    //console.log(this.message);
+    if(this.message === ""){
+      console.log("cannot submit the form!!")
+      return;
+    }
+
     fetch(api,{
       method:'POST',
       body: this.message,
--- SpringBootBackend/src/main/java/com/fullstackdevelopment/springbootstarter/reservation/MakeReservation.java
+++ SpringBootBackend/src/main/java/com/fullstackdevelopment/springbootstarter/reservation/MakeReservation.java
@@ -1,5 +1,7 @@
 package com.fullstackdevelopment.springbootstarter.reservation;
 
+import javax.mail.MessagingException;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/makeReservation/")
 public class MakeReservation{	
-
+	
 	@CrossOrigin(origins = "*")
 	@RequestMapping(method = RequestMethod.POST)
 	public String makeReservation(@RequestBody String message) {
-		System.out.println(message);	
-		return "Done!!";
+		
+		// assigning sender and receiver email addresses
+		String from = "ravintola.himalayankitchen@gmail.com";
+		String to  = "surendra.pandey2@metropolia.fi";
+		boolean messageSent = false;
+		
+		if(!message.isEmpty()) {
+			ReservationThroughEmail makeReservation = new ReservationThroughEmail(message, to, from);
+			try {
+				messageSent = makeReservation.sendMail();
+			} catch (MessagingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println((messageSent) ?  "Message Successfully sent!!" : "Message Sending failed!!");
+		return (messageSent) ?  "Message Successfully sent!!" : "Message Sending failed!!";
 	}
 }
