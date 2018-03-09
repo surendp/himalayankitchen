432ec8e3d7d95188773ac07a830ea0a628975b9b
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-03-09 12:23:52 +0200

    Refactored FetchAPI component. Code breakdown to multiple functions. Comments added

--- ReactFrontend/app/components/body/lunch&menu/modelcomponents/fetchApi.jsx
+++ ReactFrontend/app/components/body/lunch&menu/modelcomponents/fetchApi.jsx
@@ -2,39 +2,63 @@ import React from 'react';
 import Dish from './dish.jsx';
 import ContentDiv from '../../content/contentDiv.jsx';
 
+/*
+* Fetches an api
+* Creates Dish objects from the fetched data
+* stores the Objects in an array
+* Saves the array in its state
+* Finally, renders the Dishes from its state in the form of unordered list
+*/
+
 class FetchAPI extends React.Component{
 
   constructor(props){
     super(props);
-
     // store dishes fetched from the api
     this.state = {
       dishes: []
     };
-
   }
 
-
+  // Life cycle method where data is fetched
   componentDidMount(){
-    fetch(this.props.api)
+    //using fetchAndReturn method
+    let dishes = this.fetchAndReturn(this.props.api);
+    console.log(dishes);
+    // save the dishes in the state
+    //this.setState({dishes:dishes});
+  }
+
+  //Fetch the api passed as a property of this component
+  //Return an array of Dish components
+  fetchAndReturn(api){
+
+    return fetch(api)
       .then(result => result.json())
       .then(
         (data) => {
           let dishes = data.map((dish) => {
-            return <Dish
-                        name = {dish.name}
-                        catagory = {dish.catagory}
-                        price = {dish.price}
-                        description = {dish.description}
-                    />
+            // using returnDishComponent method
+            return this.returnDishComponent(dish);
           })
-
+          // save the dishes to the state of this component
           this.setState({dishes:dishes});
+          return dishes;
         }
       )
-
   }
 
+  // return a Dish component
+  returnDishComponent(dish){
+    return <Dish
+                name = {dish.name}
+                catagory = {dish.catagory}
+                price = {dish.price}
+                description = {dish.description}
+            />
+  }
+
+  // returns a list of dishes from an array
   renderDishes(){
     const dishes = this.state.dishes.map((dish, index) => {
       return( <li key={index}>
@@ -45,6 +69,7 @@ class FetchAPI extends React.Component{
     return dishes;
   }
 
+  // creates unordered list and renders the data
   render(){
     let dishList = <div>
                       <ul>{this.renderDishes()}</ul>
