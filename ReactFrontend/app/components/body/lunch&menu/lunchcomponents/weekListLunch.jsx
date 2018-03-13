5bbba00fb029fa9bf15d0e6479a59b001bdaca59
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-03-13 21:22:40 +0200

    Lunch list modified(weekLunchList.jsx). Weekly Lunch list is displayed starting from todays luch

--- ReactFrontend/app/components/body/lunch&menu/lunchcomponents/weekListLunch.jsx
+++ ReactFrontend/app/components/body/lunch&menu/lunchcomponents/weekListLunch.jsx
@@ -10,26 +10,55 @@ import FridayLunch from './fridayLunch.jsx';
 import SaturdayLunch from './saturdayLunch.jsx';
 
 
-class WeekListLunch extends React.Component{
+// array of week days lunch list components
+const weekDays = [<MondayLunch /> ,<TuesdayLunch />,<WednesdayLunch />,<ThursdayLunch />,<FridayLunch />,
+                  <MondayLunch /> ,<TuesdayLunch />,<WednesdayLunch />,<ThursdayLunch />,<FridayLunch />];
 
-  renderList(){
-    return(
-      <div>
-        <MondayLunch />
-        <TuesdayLunch />
-        <WednesdayLunch />
-        <ThursdayLunch />
-        <FridayLunch />
-        <SaturdayLunch />
-      </div>
-    )
-  }
+  /*
+  * Create list of lunch Components
+  * Render the created lunch list
+  */
+  class WeekListLunch extends React.Component{
 
-  render(){
-    return(
-      this.renderList()
-    )
-  }
+
+    // create list to display
+    createLunchList(){
+      let date = new Date();
+      let day = date.getDay();
+
+      // return the lunch list
+      return this.getList(day);
+    }
+
+    // get the list starting from today cronologically
+    getList(day){
+        if(day < 6){
+          return weekDays.slice(day-1, (day+5)-1);
+        }
+
+        return weekDays.slice(0,5);
+    }
+
+    // return the list of components from array
+    renderList(){
+      let lunch = this.createLunchList();
+      return(
+        // convert array into list
+        lunch.map((lunchList, index) => {
+          return <li key = {index}>{lunchList}</li>
+        })
+      )
+    }
+
+    render(){
+      return(
+        <div>
+          <ul>
+            {this.renderList()}
+          </ul>
+        </div>
+      )
+    }
 }
 
 export default WeekListLunch;
